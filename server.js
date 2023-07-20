77365508685fe2cc138ee83109ff3381e588e6fe
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-07-20 15:41:08 -0400

    now that db is working, I am cleaning up the server and start with inquirer... then I will connect mysql once I get inquirere working

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //requiring resources
 const inquirer = require('inquirer');
 const express = require('express');
-// const mysql = require('mysql2');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -9,31 +9,9 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// const db = mysql.createConnection(
-//   {
-//     host: 'localhost',
-//     // MySQL username,
-//     user: 'root',
-//     // MySQL password
-//     password: 'cli$ession4My$ql',
-//     database: 'employees'
-//   },
-//   console.log(`Connected to the database.`)
-// );
 
 inquirer
   .prompt([
-    // {
-    //     type: 'input',
-    //     name: 'name',
-    //     message: 'What is your name?',
-    //   },
-    //   {
-    //     type: 'checkbox',
-    //     message: 'What languages do you know?',
-    //     name: 'stack',
-    //     choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
-    //   },
     {
       type: 'list',
       message: 'What woudl you like to do?',
@@ -41,19 +19,6 @@ inquirer
       choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
     }
   ]).then((data) => {
-    //do something with the responses
-    //for example, call query nodes based on response, or open new inquirer
-    const overallAction = data.overall.toLowerCase().replace(/ /g, "");
-    console.log(overallAction);
+
   });
 
-
-
-// to catch all unfulfilled requests
-// app.use((req, res) => {
-//   res.status(404).end();
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
