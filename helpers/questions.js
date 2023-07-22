c068b9d1eac4f4925ccb754fd0ee461b52c73b06
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-07-21 22:09:21 -0400

    jumping in between commits to find a piece of code i deleted

---
+++ helpers/questions.js
@@ -0,0 +1,10 @@
+const questions = [
+    {
+    type: 'list',
+    message: 'What woudl you like to do?',
+    name: 'overall',
+    choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department'],
+  }
+];
+
+module.exports = questions;
