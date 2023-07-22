f436548360a10f8f3c31d265dd9e18906f0fd0ee
Sam Azimi <128939531+dinozio-design@users.noreply.github.com>
2023-07-22 16:38:30 -0400

    YESSSSS! the first query runs smooth. a nd table is presentable need to clean up the columns and remove unneded columns and add all VIEW QUERIES

--- helpers/query.js
+++ helpers/query.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 async function viewAllEmployees(db) {
     try {
         const result = await db.promise().query('SELECT * FROM employee');
-        console.log(result);
+        console.table(result[0]);
     } catch (error) {
         
     }
